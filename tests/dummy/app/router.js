0582489dd29ded7b7ffbb8bfbdac801f672ba42a
Marten Schilstra <mail@martenschilstra.nl>
2016-05-11 10:49:25 +0200

    Basic structure for docs site

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,6 +6,17 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('documentation', function() {
+    this.route('index', { path: '/' });
+    this.route('installation');
+    this.route('quickstart');
+
+    this.route('one-way-input');
+    this.route('one-way-textarea');
+    this.route('one-way-checkbox');
+    this.route('one-way-radio');
+    this.route('one-way-select');
+  });
 });
 
 export default Router;
